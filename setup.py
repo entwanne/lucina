32df24fbe6767e03b926dc363f68cd391f4d760c
Antoine Rozo <antoine.rozo@gmail.com>
2019-10-17 09:33:36 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lucina',
+    version='0.0.1',
+    description='From markdown to Jupyter notebook',
+    long_description='',
+    url='https://pypi.org/project/lucina/',
+    author='entwanne',
+    author_email='antoine.rozo@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
