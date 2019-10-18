cf7cdc0f747bc62f2a08d73915cf81546f265a28
Antoine Rozo <antoine.rozo@gmail.com>
2019-10-18 14:12:50 +0200

    Add metadata to setup.py

--- lucina/__init__.py
+++ lucina/__init__.py
@@ -1,6 +1,15 @@
+from lucina._about import __author__
+from lucina._about import __author_email__
+from lucina._about import __license__
+from lucina._about import __url__
+from lucina._about import __version__
 from lucina.cell import Cell
 from lucina.format import format_doc
 from lucina.parser import parse_cells
 from lucina.tokenizer import tokenize_files
 
-__all__ = ['Cell', 'format_doc', 'parse_cells', 'tokenize_files']
+
+__all__ = [
+    '__author__', '__author_email__', '__license__', '__url__', '__version__',
+    'Cell', 'format_doc', 'parse_cells', 'tokenize_files',
+]
---
+++ lucina/_about.py
@@ -0,0 +1,5 @@
+__author__ = 'entwanne'
+__author_email__ = 'antoine.rozo@gmail.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/entwanne/lucina'
+__version__ = '0.0.1'
